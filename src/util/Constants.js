f24ef09e1b2b4b2e4369894fba9605f1ee0986db
Rezza Anwary <rezza_anwary@yahoo.com>
2024-03-30 20:21:16 +0000

    update version @2.2412.54

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -7,7 +7,7 @@ exports.DefaultOptions = {
         headless: true,
         defaultViewport: null
     },
-    webVersion: '2.2412.51',
+    webVersion: '2.2412.54',
     webVersionCache: {
         type: 'local',
     },
