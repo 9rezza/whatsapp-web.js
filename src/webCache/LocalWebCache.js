397e3ed912bb7f145958351f0640e0933cd45476
Rezza Anwary <rezza_anwary@yahoo.com>
2024-03-30 13:49:15 +0000

    Detect version using finders

--- src/webCache/LocalWebCache.js
+++ src/webCache/LocalWebCache.js
@@ -30,15 +30,17 @@ class LocalWebCache extends WebCache {
     }
 
     async persist(indexHtml) {
-        // extract version from index (e.g. manifest-2.2206.9.json -> 2.2206.9)
-        const versionData = indexHtml.match(/manifest-([\d\\.]+)\.json/);
-        
-        let version = null;
-        if (versionData) {
-            version = indexHtml.match(/manifest-([\d\\.]+)\.json/)[1];
-        }
-        
-        if(!version) return;
+        // extract version from index (e.g. manifest-2.2206.9.json -> 2.2206.9)        
+        // extract version from client_revision (e.g. 1012170943 -> 2.3000.1012170943)
+		let version = null;
+		const matches = indexHtml.match(/"client_revision"\s*:\s*(\d+)/) || [];
+		if (matches[1]) {
+			version = `2.3000.${matches[1]}`;
+		}else{
+			version = indexHtml.match(/manifest-([\d\\.]+)\.json/)[1];
+		}
+
+		if(!version) return;
    
         const filePath = path.join(this.path, `${version}.html`);
         fs.mkdirSync(this.path, { recursive: true });
